330aafdb7168f5d82ee4b452bb52105c98b9ec1f
joohyun28 <63608587+joohyun28@users.noreply.github.com>
2020-04-14 04:33:02 +0900

    Add files via upload

---
+++ 28기 이주현 과제 1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#include<string>
+
+using namespace std;
+
+int main() {
+
+	string name;
+	cout << "이름을 입력하세요:";
+	getline(cin, name);
+
+	string birth;
+	cout << "생일을 입력하세요:";
+	getline(cin, name);
+
+	string age;
+	cout << "나이를 입력하세요:";
+	getline(cin, age);
+
+	string department;
+	cout << "소속 학과를 입력하세요:";
+	getline(cin, department);
+
+	cout << endl << "=====================================" << endl << "<회원정보>" << endl;
+	cout << "이름:" << name << endl;
+	cout << "생일:" << birth << endl;
+	cout << "나이:" << age << endl;
+	cout << "소속 학과:" << department << endl;
+
+}
---
+++ 28기 이주현 과제2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+
+
+	float num1, num2;
+
+cout << "First number:";
+cin >> num1;
+cout << "Second number:";
+cin >> num2;
+
+cout << num1 << '+' << num2 << '=' << num1 + num2 << endl;
+cout << num1 << '-' << num2 << '=' << num1 - num2 << endl;
+cout << num1 << '*' << num2 << '=' << num1 * num2 << endl;
+cout << num1 << '/' << num2 << '=' << num1 / num2 << endl;
+
+
+}
